5065f3d12779c153927f98e9546d0c563b88d4f2
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-04-18 14:41:27 +0200

    Project Init
    Created project and login form

--- HealthCare System/HealthCare System/MainWindow.xaml.cs
+++ HealthCare System/HealthCare System/MainWindow.xaml.cs
@@ -8,69 +8,8 @@ namespace HealthCare_System
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    class Osoba
-    {
-        
-    
-
-        private string firstName;
-
-        public string FirstName
-        {
-            get { return firstName; }
-            set { firstName = value; }
-        }
-
-        private string lastName;
-
-        public string LastName
-        {
-            get { return lastName; }
-            set { lastName = value; }
-        }
-        private string mail;
-
-        public string Mail
-        {
-            get { return mail; }
-            set { mail = value; }
-        }
-        private string password;
-
-        public string Password
-        {
-            get { return password; }
-            set { password = value; }
-        }
-        private string birthDate;
-
-        public string BirthDate
-        {
-            get { return birthDate; }
-            set { birthDate = value; }
-        }
-
-
-
-
-
-
-
-
-    }
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            List<Osoba> persons = JsonConvert.DeserializeObject<List<Osoba>>(File.ReadAllText("../../../data/usersLogin.json"));
-            for(int i = 0; i < persons.Count; i++)
-            {
-                lb.Items.Add(persons[i].Mail);
-            }
-
-        }
-
         private void loginBtn_Click(object sender, RoutedEventArgs e)
         {
             string mail = mailTb.Text;
