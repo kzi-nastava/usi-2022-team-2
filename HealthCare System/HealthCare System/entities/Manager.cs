ca63880b5f54e34addd691869035dd6fa1009e73
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2022-04-19 19:52:38 +0200

    Created empty manager class and main window for manager

---
+++ HealthCare System/HealthCare System/entities/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare_System.entities
+{
+    class Manager : Person
+    {
+        public Manager()
+        {
+
+        }
+
+        public Manager(string firstName, string lastName, DateTime birthDate, string mail, string password) : base(firstName, lastName, birthDate, mail, password)
+        {
+        }
+
+        public Manager(Manager manager) : base(manager.FirstName, manager.LastName, manager.BirthDate, manager.Mail, manager.Password)
+        {
+
+        }
+    }
+}
