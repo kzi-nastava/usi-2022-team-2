0a8a857d0bf4e1ec0ca0877e7a6654cc1ef61e5f
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2022-04-20 19:59:10 +0200

    Implented classes
    Implemented constutors, properties and overridden toString method of following classes: Room, Equipment, Renovation, SimpleRenovation, MergingRenovation, SplittingRenovation, Drug, Ingredient

--- HealthCare System/HealthCare System/entities/MergingRenovation.cs
+++ HealthCare System/HealthCare System/entities/MergingRenovation.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace HealthCare_System.entities
 {
     class MergingRenovation : Renovation
     {
+        List<Room> rooms;
+
+        public MergingRenovation() { }
+
+        public MergingRenovation(Renovation renovation) : base(renovation)
+        {
+            this.rooms = null;
+        }
+
+        public MergingRenovation(int id, DateTime beginningDate, DateTime endingDate) : base(id, beginningDate, endingDate)
+        {
+            this.rooms = null;
+        }
+
+        public MergingRenovation(int id, DateTime beginningDate, DateTime endingDate, List<Room> rooms) : base(id, beginningDate, endingDate)
+        {
+            this.rooms = rooms;
+        }
+
+        public MergingRenovation(MergingRenovation renovation) : base(renovation)
+        {
+            this.rooms = renovation.Rooms;
+        }
+
+        [JsonIgnore]
+        public List<Room> Rooms { get => rooms; set => rooms = value; }
+
+        public override string ToString()
+        {
+            return "MergingRenovation" + base.ToString();
+        }
     }
 }
--- HealthCare System/HealthCare System/entities/SplittingRenovation.cs
+++ HealthCare System/HealthCare System/entities/SplittingRenovation.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace HealthCare_System.entities
 {
     class SplittingRenovation : Renovation
     {
+        Room room;
+
+        public SplittingRenovation() { }
+
+        public SplittingRenovation(Renovation renovation) : base(renovation)
+        {
+            this.room = null;
+        }
+
+        public SplittingRenovation(int id, DateTime beginningDate, DateTime endingDate) : base(id, beginningDate, endingDate)
+        {
+            this.room = null;
+        }
+
+        public SplittingRenovation(int id, DateTime beginningDate, DateTime endingDate, Room room) : base(id, beginningDate, endingDate)
+        {
+            this.room = room;
+        }
+
+        public SplittingRenovation(SplittingRenovation renovation) : base(renovation)
+        {
+            this.room = renovation.room;
+        }
+
+        [JsonIgnore]
+        public Room Room { get => room; set => room = value; }
+
+        public override string ToString()
+        {
+            return "SplittingRenovation" + base.ToString();
+        }
     }
 }
