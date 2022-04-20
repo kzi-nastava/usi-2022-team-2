b34606d982a9583bccfc59b836dd99496423b2e2
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2022-04-20 10:41:01 +0200

    Created manager controller

---
+++ HealthCare System/HealthCare System/controllers/ManagerController.cs
@@ -0,0 +1,40 @@
+﻿using HealthCare_System.entities;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare_System.controllers
+{
+    class ManagerController
+    {
+        List<Manager> managers;
+
+        public ManagerController()
+        {
+            this.LoadManagers();
+        }
+
+        public List<Manager> Managers
+        {
+            get { return managers; }
+            set { managers = value; }
+        }
+
+        void LoadManagers()
+        {
+            this.managers = JsonConvert.DeserializeObject<List<Manager>>(File.ReadAllText("data/entities/Appointments.json"));
+        }
+
+        public Manager FindByMail(string mail)
+        {
+            foreach (Manager manager in this.managers)
+                if (manager.Mail == mail)
+                    return manager;
+            return null;
+        }
+    }
+}
