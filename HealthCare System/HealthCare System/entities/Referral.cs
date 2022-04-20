3ad412669106959d9449615a216e113e3e4023c2
cuturic01 <miloscuturic8@gmail.com>
2022-04-20 19:56:19 +0200

    Implemented classes
    Implemented constructors, properties and overridden ToString method of HospitalSurvey, DoctorSurvey, MedicalRecord, Appointment, Doctor, Anamnesis, Referral.

--- HealthCare System/HealthCare System/entities/Referral.cs
+++ HealthCare System/HealthCare System/entities/Referral.cs
@@ -2,11 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HealthCare_System.entities
 {
     class Referral
     {
+        int id;
+        Specialization specialization;
+        Doctor doctor;
+        Patient patient;
+        bool used;
+
+        public Referral() { }
+
+        public Referral(int id, Specialization specialization, Doctor doctor, Patient patient, bool used)
+        {
+            this.id = id;
+            this.specialization = specialization;
+            this.doctor = doctor;
+            this.patient = patient;
+            this.used = used;
+        }
+
+        public Referral(int id, Specialization specialization, bool used)
+        {
+            this.id = id;
+            this.specialization = specialization;
+            this.used = used;
+        }
+
+        public Referral(Referral referral)
+        {
+            id = referral.id;
+            specialization = referral.specialization;
+            doctor = referral.doctor;
+            patient = referral.patient;
+            used = referral.used;
+        }
+
+        [JsonPropertyName("id")]
+        public int Id { get => id; set => id = value; }
+
+        [JsonPropertyName("used")]
+        public bool Used { get => used; set => used = value; }
+
+        [JsonPropertyName("specialization")]
+        internal Specialization Specialization { get => specialization; set => specialization = value; }
+
+        [JsonIgnore]
+        internal Doctor Doctor { get => doctor; set => doctor = value; }
+
+        [JsonIgnore]
+        internal Patient Patient { get => patient; set => patient = value; }
+
+        public override string ToString()
+        {
+            string doctorInfo;
+            if (doctor is null) doctorInfo = "null";
+            else doctorInfo = Doctor.Jmbg;
+
+            string patientInfo;
+            if (patient is null) patientInfo = "null";
+            else patientInfo = Patient.Jmbg;
+
+            return "Refferal[" + "id: " + this.id + ", used: " + this.used + ", specialization: " + this.specialization.ToString() + 
+                ", doctor: " + doctorInfo +", patient: " + patientInfo + "]";
+        }
     }
 }
