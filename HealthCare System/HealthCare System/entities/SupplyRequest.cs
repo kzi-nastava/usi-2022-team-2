6467330db7c9d0dc2ba18bc33740d1920b301459
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-04-21 01:03:34 +0200

    Added Class SupplyRequest

--- HealthCare System/HealthCare System/entities/SupplyRequest.cs
+++ HealthCare System/HealthCare System/entities/SupplyRequest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HealthCare_System.entities
@@ -10,5 +11,50 @@ namespace HealthCare_System.entities
     {
         int id;
         DateTime requestCreated;
+        Dictionary<Equipment, int> orderDetails;
+
+        public SupplyRequest() { }
+
+        public SupplyRequest(int id, DateTime requestCreated)
+        {
+            this.id = id;
+            this.requestCreated = requestCreated;
+        }
+
+        public SupplyRequest(int id, DateTime requestCreated, Dictionary<Equipment, int> orderDetails)
+        {
+            this.id = id;
+            this.requestCreated = requestCreated;
+            this.orderDetails = orderDetails;
+        }
+
+        public SupplyRequest(SupplyRequest request)
+        {
+            this.id = request.id;
+            this.requestCreated = request.requestCreated;
+            this.orderDetails = request.orderDetails;
+        }
+
+        [JsonPropertyName("id")]
+        public int Id { get => id; set => id = value; }
+        [JsonPropertyName("requestCreated")]
+        public DateTime RequestCreated { get => requestCreated; set => requestCreated = value; }
+        [JsonIgnore]
+        internal Dictionary<Equipment, int> OrderDetails { get => orderDetails; set => orderDetails = value; }
+
+        public override string ToString()
+        {
+            string orders="{";
+            bool firstEntry = true;
+            foreach (KeyValuePair<Equipment, int> equipment in this.orderDetails)
+            {
+                if (firstEntry) firstEntry = false;
+                else orders += ", ";
+                orders += equipment.Key + ":" + equipment.Value;
+            }
+            orders += "}";
+            return "DrugNotification[" + "id: " + this.Id.ToString() +
+                ", requestCreated: " + this.requestCreated.ToString("dd/MM/yyyy HH:mm") + ", orderDetails: " + orders + "]";
+        }
     }
 }
