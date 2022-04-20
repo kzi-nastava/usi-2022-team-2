2c0b63a9a2476255a84ed03b8012a370c2bb7dc3
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-04-20 17:44:22 +0200

    Added Empty Classes

---
+++ HealthCare System/HealthCare System/entities/DaysOffRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare_System.entities
+{
+    public enum DaysOffRequestState { WAITING, ACCEPTED, DENIED}
+    class DaysOffRequest
+    {
+        DateTime start;
+        DateTime end;
+        String description;
+        DaysOffRequestState state;
+        bool urgent;
+        Doctor doctor;
+
+
+    }
+}
