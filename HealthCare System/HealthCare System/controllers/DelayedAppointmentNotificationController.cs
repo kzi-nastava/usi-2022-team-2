62ee7cbef6c1ec608f84276493540f4927c496dd
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-04-22 15:06:37 +0200

    Added Controllers

---
+++ HealthCare System/HealthCare System/controllers/DelayedAppointmentNotificationController.cs
@@ -0,0 +1,40 @@
+﻿using HealthCare_System.entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HealthCare_System.controllers
+{
+    class DelayedAppointmentNotificationController
+    {
+        List<DelayedAppointmentNotification> delayedAppointmentNotifications;
+
+        public DelayedAppointmentNotificationController()
+        {
+            this.LoadDelayedAppointmentNotifications();
+        }
+
+        public List<DelayedAppointmentNotification> DelayedAppointmentNotifications
+        {
+            get { return delayedAppointmentNotifications; }
+            set { delayedAppointmentNotifications = value; }
+        }
+
+        void LoadDelayedAppointmentNotifications()
+        {
+            this.delayedAppointmentNotifications = JsonSerializer.Deserialize<List<DelayedAppointmentNotification>>(File.ReadAllText("data/entities/Drugs.json"));
+        }
+
+        public DelayedAppointmentNotification FindById(int id)
+        {
+            foreach (DelayedAppointmentNotification delayedAppointmentNotification in this.delayedAppointmentNotifications)
+                if (delayedAppointmentNotification.Id == id)
+                    return delayedAppointmentNotification;
+            return null;
+        }
+    }
+}
---
+++ HealthCare System/HealthCare System/controllers/EquipmentControllers.cs
@@ -0,0 +1,40 @@
+﻿using HealthCare_System.entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HealthCare_System.controllers
+{
+    class EquipmentControllers
+    {
+        List<Equipment> equipments;
+
+        public EquipmentControllers()
+        {
+            this.LoadEquipment();
+        }
+
+        public List<Equipment> Equipment
+        {
+            get { return equipments; }
+            set { equipments = value; }
+        }
+
+        void LoadEquipment()
+        {
+            this.equipments = JsonSerializer.Deserialize<List<Equipment>>(File.ReadAllText("data/entities/Equipment.json"));
+        }
+
+        public Equipment FindById(int id)
+        {
+            foreach (Equipment equipment in this.equipments)
+                if (equipment.Id == id)
+                    return equipment;
+            return null;
+        }
+    }
+}
