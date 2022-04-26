bc2b9908cd5d7abec41315d142a50a5f5d2896c8
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-04-26 16:26:40 +0200

    Added new class Secretary and controller

---
+++ HealthCare System/HealthCare System/controllers/SecretaryController.cs
@@ -0,0 +1,35 @@
+﻿using HealthCare_System.entities;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HealthCare_System.controllers
+{
+    class SecretaryController
+    {
+        List<Secretary> secretaries;
+        string path;
+
+        public SecretaryController()
+        {
+            path = "data/entities/Secretaries.json";
+            Load();
+        }
+
+        public SecretaryController(string path)
+        {
+            this.path = path;
+            Load();
+        }
+
+        public string Path { get => path; set => path = value; }
+
+        internal List<Secretary> Secretary { get => secretaries; set => secretaries = value; }
+
+
+        void Load()
+        {
+            secretaries = JsonSerializer.Deserialize<List<Secretary>>(File.ReadAllText(this.path));
+        }
+    }
+}
---
+++ HealthCare System/HealthCare System/entities/Secretary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare_System.entities
+{
+    class Secretary : Person
+    {
+
+        public Secretary(string jmbg, string firstName, string lastName, DateTime birthDate, 
+            string mail, string password) : base(jmbg, firstName, lastName, birthDate, mail, password)
+        {
+
+        }
+
+        public Secretary(Secretary secretary) : base(secretary.Jmbg, secretary.FirstName, secretary.LastName,
+            secretary.BirthDate, secretary.Mail, secretary.Password) { }
+    }
+}
