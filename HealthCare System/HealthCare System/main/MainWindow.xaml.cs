9c7310ee2ea13b6b51c0fb75e7741815ac50f420
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2022-04-28 00:49:29 +0200

    Added Logout Function And Some Improvements

--- HealthCare System/HealthCare System/main/MainWindow.xaml.cs
+++ HealthCare System/HealthCare System/main/MainWindow.xaml.cs
@@ -12,14 +12,25 @@ namespace HealthCare_System
     /// </summary>
     public partial class MainWindow : Window
     {
-        HealthCareFactory factory = new();
+        HealthCareFactory factory;
         
+        public MainWindow(HealthCareFactory factory)
+        {
+            this.factory = factory;
+            InitializeComponent();
+        }
+        public MainWindow()
+        {
+            factory = new();
+            InitializeComponent();
+        }
         private void LoginBtn_Click(object sender, RoutedEventArgs e)
         {
             string mail = mailTb.Text;
             string password = passwordTb.Password;
             factory.PrintContnent();
             Person person = factory.Login(mail, password);
+            Console.WriteLine( factory.DoctorController.FindByJmbg("1001").IsAvailable(new DateTime(2022, 4, 25, 13, 01, 0), new DateTime(2022, 4, 25, 13, 16, 0)));
 
             //TODO add the rest of user types
             if (person is null)
@@ -36,6 +47,7 @@ namespace HealthCare_System
                 factory.User = person;
                 PatientWindow patientWindow = new PatientWindow(factory);
                 patientWindow.Show();
+                this.Close();
             }
             else if (person.GetType() == typeof(Manager)) {
                 Window managerWindow = new ManagerWindow(factory);
