f6636689849d3ecfdf88040d94d0d6d082a2c2ae
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-04-30 02:49:00 +0200

    Fixed appointment request gui bug

--- HealthCare System/HealthCare System/gui/RequestDetailsWindow.xaml.cs
+++ HealthCare System/HealthCare System/gui/RequestDetailsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HealthCare_System.entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,19 @@ namespace HealthCare_System.gui
     /// </summary>
     public partial class RequestDetailsWindow : Window
     {
-        public RequestDetailsWindow()
+        public RequestDetailsWindow(AppointmentRequest request)
         {
             InitializeComponent();
+            textBlockDoctor.Text = request.Appointment.Doctor.FirstName + " " + request.Appointment.Doctor.LastName;
+            textBlockPatient.Text = request.Patient.FirstName + " " + request.Patient.LastName;
+            textBlockTime.Text = request.RequestCreated.ToString();
+            textBlockType.Text = request.Type.ToString();
+            textBlockStatus.Text = request.State.ToString();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
