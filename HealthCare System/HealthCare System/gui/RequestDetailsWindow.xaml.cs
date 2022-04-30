1f0c0887776377885fa1e29311e5ae5f5f0d3633
StjepanovicSrdjan <93882357+StjepanovicSrdjan@users.noreply.github.com>
2022-04-30 02:48:06 +0200

    Finished appointment request review gui, 4.3

---
+++ HealthCare System/HealthCare System/gui/RequestDetailsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HealthCare_System.gui
+{
+    /// <summary>
+    /// Interaction logic for RequestDetailsWindow.xaml
+    /// </summary>
+    public partial class RequestDetailsWindow : Window
+    {
+        public RequestDetailsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
